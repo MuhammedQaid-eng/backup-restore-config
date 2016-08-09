7585bb6e40199f5e6a821ce387999384ab88c432
andela-ayusuf <abdulmalik.yusuf@andela.com>
2016-08-09 14:31:34 +0100

    add dummy variables

--- features/step_definitions/vars.rb
+++ features/step_definitions/vars.rb
@@ -1,1 +1,3 @@
-BUCKETNAME = "testcronbackbuk"
+BUCKETNAME = "S3 BUCKETNAME"
+PATHTOKEY = "/PATH/TO/PRIVATE/KEY"
+PUBDNS = "PUBLIC DNS" #e.g. ubuntu@ec2-12-345-678-90.us-west-1.compute.amazonaws.com
